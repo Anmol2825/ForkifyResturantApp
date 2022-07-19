d572d31c24fe9f229e9f29db4c5298833772c09a
Anmol Agarwal <mailanmolat@gmail.com>
2022-07-19 15:15:09 +0530

    Pagination

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js'
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -45,15 +46,26 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     //3)Render results
-    resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage(1));
+
+    //4) Render initil pagintion button
+    paginationView.render(model.state.search);
   } catch (err) {
     // console.log(err);
   }
 };
 
+const controlPagination = function (goToPage) {
+  //1)Render New Results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  //2)Render initial pagination button 
+  paginationView.render(model.state.search);
+}
 
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 init();
